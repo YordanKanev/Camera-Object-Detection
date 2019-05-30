c10e6f6053d610ae108ecb947061a763a50084b4
Georgi Samardzhiev <gvsamardzh@uni-sofia.bg>
2019-05-30 19:51:55 +0300

    Working camera

--- camera/camera.ino
+++ camera/camera.ino
@@ -1,9 +1,17 @@
 #include "esp_camera.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <base64.h>
 #include "esp_http_server.h"
 #include "esp_timer.h"
-#include <base64.h>
+#include "img_converters.h"
+#include "camera_index.h"
+#include "Arduino.h"
+
+#include "fb_gfx.h"
+#include "fd_forward.h"
+#include "dl_lib.h"
+#include "fr_forward.h"
 
 //
 // WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
@@ -15,119 +23,113 @@
 //#define CAMERA_MODEL_M5STACK_PSRAM
 #define CAMERA_MODEL_AI_THINKER
 
-const char* ssid = "FMI-AIR-NEW";
-const char* password = "";
+const char *ssid = "FMI-AIR-NEW";
+const char *password = "";
 #define SENSOR GPIO_NUM_15
 #define BAUD_RATE 115200
-#define EXT_WAKEUP_PIN_BITMASK 0x1000  //  2^12
+#define EXT_WAKEUP_PIN_BITMASK 0x1000 //  2^12
 #define MINIMUM_WAKE_PERIOD_MILLIS 60e3
 
 #if defined(CAMERA_MODEL_WROVER_KIT)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 21
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
 
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 19
+#define Y4_GPIO_NUM 18
+#define Y3_GPIO_NUM 5
+#define Y2_GPIO_NUM 4
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 #elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    15
-#define XCLK_GPIO_NUM     27
-#define SIOD_GPIO_NUM     25
-#define SIOC_GPIO_NUM     23
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM 27
+#define SIOD_GPIO_NUM 25
+#define SIOC_GPIO_NUM 23
 
-#define Y9_GPIO_NUM       19
-#define Y8_GPIO_NUM       36
-#define Y7_GPIO_NUM       18
-#define Y6_GPIO_NUM       39
-#define Y5_GPIO_NUM        5
-#define Y4_GPIO_NUM       34
-#define Y3_GPIO_NUM       35
-#define Y2_GPIO_NUM       32
-#define VSYNC_GPIO_NUM    22
-#define HREF_GPIO_NUM     26
-#define PCLK_GPIO_NUM     21
+#define Y9_GPIO_NUM 19
+#define Y8_GPIO_NUM 36
+#define Y7_GPIO_NUM 18
+#define Y6_GPIO_NUM 39
+#define Y5_GPIO_NUM 5
+#define Y4_GPIO_NUM 34
+#define Y3_GPIO_NUM 35
+#define Y2_GPIO_NUM 32
+#define VSYNC_GPIO_NUM 22
+#define HREF_GPIO_NUM 26
+#define PCLK_GPIO_NUM 21
 
 #elif defined(CAMERA_MODEL_AI_THINKER)
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 #else
 #error "Camera model not selected"
 #endif
 
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
-void sendPhoto(camera_fb_t * fb, int dev_id) {
-  if (WiFi.status()== WL_CONNECTED) {   //Check WiFi connection status
+static mtmn_config_t mtmn_config = {0};
 
-    HTTPClient http;
-    http.begin("https://exerceo.serveo.net/device/");  //Specify destination for HTTP request
-    http.addHeader("Content-Type", "application/json");  //Specify content-type header
+static int8_t detection_enabled = 1;
+static int8_t recognition_enabled = 0;
+static int8_t is_enrolling = 0;
+static face_id_list id_list = {0};
 
-    String payload = "{\"deviceId\" : \""; 
-    payload += dev_id;
-    payload += "\",\"image\" : \"data:image/jpeg;base64,";
-    payload += base64::encode(fb->buf, fb->len);
-    payload += "\"}";
-    Serial.println("send");
-    int httpResponseCode = http.POST(payload);   //Send the actual POST request
+void startCameraServer();
 
-    if (httpResponseCode>0) {
-      String response = http.getString();                       //Get the response to the request
-
-      Serial.println(httpResponseCode);   //Print return code
-      Serial.println(response);           //Print request answer
-    
-    } else {
-      
-      Serial.print("Error on sending POST: ");
-      Serial.println(http.getString());
-      Serial.println(httpResponseCode);
-    }
-    http.end();  //Free resources
-  } else {
-    Serial.println("Error in WiFi connection");
-  }
-
-}
-
-
-void startCameraServer(void(*sendPhoto)(camera_fb_t*, int));
-
-void motionDetected(){
+void motionDetected()
+{
   Serial.println("Motion detected!");
   delay(1000);
 }
 unsigned long lastWakeupPinHigh = 0;
 
-void setup() {
+camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+  char *part_buf[64];
+  dl_matrix3du_t *image_matrix = NULL;
+  bool detected = false;
+  int face_id = 0;
+  int64_t fr_start = 0;
+  int64_t fr_ready = 0;
+  int64_t fr_face = 0;
+  int64_t fr_recognize = 0;
+  int64_t fr_encode = 0;
+
+  static int64_t last_frame = 0;
+
+void setup()
+{
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
@@ -153,12 +155,27 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
+
+  mtmn_config.min_face = 80;
+    mtmn_config.pyramid = 0.7;
+    mtmn_config.p_threshold.score = 0.6;
+    mtmn_config.p_threshold.nms = 0.7;
+    mtmn_config.r_threshold.score = 0.7;
+    mtmn_config.r_threshold.nms = 0.7;
+    mtmn_config.r_threshold.candidate_number = 4;
+    mtmn_config.o_threshold.score = 0.7;
+    mtmn_config.o_threshold.nms = 0.4;
+    mtmn_config.o_threshold.candidate_number = 1;
+  
   //init with high specs to pre-allocate larger buffers
-  if (psramFound()) {
+  if (psramFound())
+  {
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
-  } else {
+  }
+  else
+  {
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
@@ -166,25 +183,27 @@ void setup() {
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
 
   //drop down frame size for higher initial frame rate
-  sensor_t * s = esp_camera_sensor_get();
+  sensor_t *s = esp_camera_sensor_get();
   s->set_framesize(s, FRAMESIZE_QVGA);
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
 
-  startCameraServer(sendPhoto);
+  startCameraServer();
 
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
@@ -193,48 +212,180 @@ void setup() {
   pinMode(SENSOR, INPUT);
   lastWakeupPinHigh = millis();
   Serial.println("PIR ready");
-  HTTPClient http;
-  http.begin("http://10.108.4.88/stream");  //Specify destination for HTTP request
-  int httpResponseCode = http.GET();   //Send the actual POST request 
 }
 
-void gotoSleep(){
+void sendPhoto(camera_fb_t *fb, int dev_id)
+{
+  Serial.println("hit send photo");
+  if (WiFi.status() == WL_CONNECTED)
+  { //Check WiFi connection status
+
+    HTTPClient http;
+
+    http.begin("http://10.108.4.85:9090/device/");      //Specify destination for HTTP request
+    http.addHeader("Content-Type", "application/json"); //Specify content-type header
+
+    String payload = "{\"deviceId\" : \"";
+    payload += dev_id;
+    payload += "\",\"image\" : \"data:image/jpeg;base64,";
+    payload += base64::encode(fb->buf, fb->len);
+    payload += "\"}";
+    Serial.println(payload);
+    Serial.println(payload[34]);
+    int httpResponseCode = http.POST(payload); //Send the actual POST request
+
+    if (httpResponseCode > 0)
+    {
+      String response = http.getString(); //Get the response to the request
+
+      Serial.println(httpResponseCode); //Print return code
+      Serial.println(response);         //Print request answer
+    }
+    else
+    {
+
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end(); //Free resources
+  }
+  else
+  {
+    Serial.println("Error in WiFi connection");
+  }
+}
+
+void gotoSleep()
+{
   Serial.println("Deep sleep enabled");
-  (SENSOR, HIGH);
+  esp_sleep_enable_ext0_wakeup(SENSOR, HIGH);
   esp_deep_sleep_start();
 }
 
-unsigned long secs() {
+unsigned long secs()
+{
   return millis() / 1e3L;
 }
 
+void startStream()
+{
+  
 
+  
+}
+void loop()
+{
 
-void loop() {
- 
+    detected = false;
+    face_id = 0;
+    fb = esp_camera_fb_get();
+    if (!fb)
+    {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    }
+    else
+    {
+      Serial.println("Start looping");
+      fr_start = esp_timer_get_time();
+      fr_ready = fr_start;
+      fr_face = fr_start;
+      fr_encode = fr_start;
+      fr_recognize = fr_start;
+      if (!detection_enabled || fb->width > 400)
+      {
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if (!jpeg_converted)
+          {
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        }
+        else
+        {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+      else
+      {
+        Serial.println("before matrix alloc");
+        image_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
 
-//  delay(7000);
-//
-//   unsigned long now=millis();
-//   int wakeupPinState=digitalRead(SENSOR);
-//   if(wakeupPinState==HIGH){
-//     lastWakeupPinHigh=now;
-//   
-//     camera_fb_t * fb = esp_camera_fb_get();
-//     if (!fb) {
-//      ESP_LOGE(TAG, "Camera Capture Failed");
-//     }
-//     //replace this with your own function
-//     sendPhoto(fb, 1);
-//
-//     //return the frame buffer back to the driver for reuse
-//     esp_camera_fb_return(fb);
-//    
-//   } else if(now-lastWakeupPinHigh >= MINIMUM_WAKE_PERIOD_MILLIS){
-//       gotoSleep();
-//   }
-//   if(now % 2000 == 0){
-//     Serial.printf("%u wakeupPinState %u \n", secs(), wakeupPinState);
-//  }
-
+        if (!image_matrix)
+        {
+          Serial.println("dl_matrix3du_alloc failed");
+          res = ESP_FAIL;
+        }
+        else
+        {
+          if (!fmt2rgb888(fb->buf, fb->len, fb->format, image_matrix->item))
+          {
+            Serial.println("fmt2rgb888 failed");
+            res = ESP_FAIL;
+          }
+          else
+          {
+            fr_ready = esp_timer_get_time();
+            box_array_t *net_boxes = NULL;
+            Serial.println("before detection_enabled check");
+            if (detection_enabled)
+            {
+              net_boxes = face_detect(image_matrix, &mtmn_config);
+            }
+            Serial.println("after face detect");
+//            fr_face = esp_timer_get_time();
+//            fr_recognize = fr_face;
+            if (net_boxes || fb->format != PIXFORMAT_JPEG)
+            {
+              if (net_boxes)
+              {
+                Serial.println("in net_boxes");
+                detected = true;
+                sendPhoto(fb,1);
+                /*if (recognition_enabled)
+                {
+                  face_id = run_face_recognition(image_matrix, net_boxes);
+                }*/
+                fr_recognize = esp_timer_get_time();
+                //draw_face_boxes(image_matrix, net_boxes, face_id);
+                free(net_boxes->box);
+                free(net_boxes->landmark);
+                free(net_boxes);
+              }
+              if (!fmt2jpg(image_matrix->item, fb->width * fb->height * 3, fb->width, fb->height, PIXFORMAT_RGB888, 90, &_jpg_buf, &_jpg_buf_len))
+              {
+                Serial.println("fmt2jpg failed");
+                res = ESP_FAIL;
+              }
+              esp_camera_fb_return(fb);
+              fb = NULL;
+            }
+            else
+            {
+              _jpg_buf = fb->buf;
+              _jpg_buf_len = fb->len;
+            }
+            fr_encode = esp_timer_get_time();
+          }
+          dl_matrix3du_free(image_matrix);
+        }
+      }
+    }
+    if (fb)
+    {
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    }
+    else if (_jpg_buf)
+    {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    Serial.printf("Photo");
 }
